bc66d4a179de3c18d2622f0005234ee3d302d702
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-01-08 21:57:35 +0300

    drop down options added.

--- DropDownPicker/ContentView.swift
+++ DropDownPicker/ContentView.swift
@@ -9,80 +9,65 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
+    @State private var selection: String = "String"
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+
+        VStack {
+
+            DropDown(content: ["String", "Int", "Double", "Float"],
+                     selection: $selection,
+                     activeTint: .primary.opacity(0.1),
+                     inActiveTint: .white.opacity(0.05))
+            .frame(width: 130)
+
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .environment(\.colorScheme, .dark)
+            .background {
+            Color(.black)
+                .ignoresSafeArea()
         }
-    }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
+
+struct DropDown: View {
+
+    var content: [String]
+    @Binding var selection: String
+    var activeTint: Color
+    var inActiveTint: Color
+
+    @State private var expandView: Bool = false
+
+    var body: some View {
+        GeometryReader {
+            let size = $0.size
+            
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach(content,id: \.self) { title in
+                    RowView(title, size)
+                    
+                }
+            }
+        }
+            .frame (height: 55)
+    }
+    
+    @ViewBuilder
+    func RowView(_ title: String, _ size: CGSize)-> some View{
+     
+        Text(title)
+            .font(.title3)
+            .fontWeight(.semibold)
+            .padding(.horizontal)
     }
 }
